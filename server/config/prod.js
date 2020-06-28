c24d346782d4141510df3dd20f0921a1c5fe1b99
mirelasteve <mirelasteve@abv.bg>
2020-06-28 19:06:22 +0300

    commit

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
     mongoURI:process.env.MONGO_URI,
     cookieKey:process.env.COOKIE_KEY,
-    googleRedirectURI:"https://infinite-hamlet-55883.herokuapp.com"
+    googleRedirectURI:"https://infinite-hamlet-55883.herokuapp.com/auth/google/callback"
 }
