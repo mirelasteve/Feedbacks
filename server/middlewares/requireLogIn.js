d7554b1c67dc8828358e45f469ee26c19e48ffbc
mirelasteve <mirelasteve@abv.bg>
2020-07-25 23:05:10 +0300

    status 401

--- server/middlewares/requireLogIn.js
+++ server/middlewares/requireLogIn.js
@@ -2,7 +2,7 @@ module.exports = (req,res,next)=>{
    
     if(!req.user){
         return res.status(401).send({
-             error:'You must log in'
+             error:'You must log in before procede'
          })
      }
      
